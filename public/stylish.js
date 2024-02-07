ea68854ece92dba38c15cc8329a675056eae4237
Héloïse P <heloise.pingitore@mmibordeaux.com>
2024-02-08 00:05:53 +0100

    Add Filter

--- public/stylish.js
+++ public/stylish.js
@@ -81,4 +81,19 @@ filterButton.addEventListener('click', () => {
 
     navFilter.classList.toggle('inactive');
     filterButton.classList.toggle('selected');
+
+
 })
+
+const characterImage = document.querySelector('.character img');
+const hueRangeInput = document.getElementById("hue-range");
+
+hueRangeInput.addEventListener("input", function() {
+    const hueValue = this.value; // Valeur de l'input range
+    const filterValue = `hue-rotate(${hueValue}deg)`; // Création de la valeur du filtre
+
+    // Appliquer la valeur du filtre à l'image
+    characterImage.style.filter = filterValue;
+    characterImage.style.webkitFilter = filterValue; // Pour les navigateurs WebKit (Safari, Chrome)
+    characterImage.style.mozFilter = filterValue; // Pour les navigateurs Mozilla (Firefox)
+});
