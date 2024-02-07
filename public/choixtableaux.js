4d1279f0d99914a1dc915d28e1db77be3eb3add9
chloaye <124672851+chloaye@users.noreply.github.com>
2024-02-07 17:17:03 +0100

    Choix Tableau FInal

---
+++ public/choixtableaux.js
@@ -0,0 +1,76 @@
+const lucrecePainting = document.getElementById('Lucrece');
+const modalLucrece = document.getElementById('modalLucrece');
+const closeLucrece = document.getElementById('closeLucrece');
+
+lucrecePainting.addEventListener('click', () => {
+    modalLucrece.classList.add('opened');
+})
+
+closeLucrece.addEventListener('click', () => {
+    modalLucrece.classList.remove('opened');
+})
+
+
+const madeleinePainting = document.getElementById('Madeleine');
+const modalMadeleine = document.getElementById('modalMadeleine');
+const closeMadeleine = document.getElementById('closeMadeleine');
+
+madeleinePainting.addEventListener('click', () => {
+    modalMadeleine.classList.add('opened');
+})
+
+closeMadeleine.addEventListener('click', () => {
+    modalMadeleine.classList.remove('opened');
+})
+
+
+const H1Painting = document.getElementById('H1');
+const modalH1 = document.getElementById('modalH1');
+const closeH1 = document.getElementById('closeH1');
+
+H1Painting.addEventListener('click', () => {
+    modalH1.classList.add('opened');
+})
+
+closeH1.addEventListener('click', () => {
+    modalH1.classList.remove('opened');
+})
+
+
+const H2Painting = document.getElementById('H2');
+const modalH2 = document.getElementById('modalH2');
+const closeH2 = document.getElementById('closeH2');
+
+H2Painting.addEventListener('click', () => {
+    modalH2.classList.add('opened');
+})
+
+closeH2.addEventListener('click', () => {
+    modalH2.classList.remove('opened');
+})
+
+
+const H3Painting = document.getElementById('H3');
+const modalH3 = document.getElementById('modalH3');
+const closeH3 = document.getElementById('closeH3');
+
+H3Painting.addEventListener('click', () => {
+    modalH3.classList.add('opened');
+})
+
+closeH3.addEventListener('click', () => {
+    modalH3.classList.remove('opened');
+})
+
+
+const H4Painting = document.getElementById('H4');
+const modalH4 = document.getElementById('modalH4');
+const closeH4 = document.getElementById('closeH4');
+
+H4Painting.addEventListener('click', () => {
+    modalH4.classList.add('opened');
+})
+
+closeH4.addEventListener('click', () => {
+    modalH4.classList.remove('opened');
+})
